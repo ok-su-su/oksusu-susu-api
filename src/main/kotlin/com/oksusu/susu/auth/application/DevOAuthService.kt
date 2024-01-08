7ba38bbc878c16ca7b91bb5740ce0f6b540acba1
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2024-01-09 08:58:41 +0900

    refac: refac oauth tx code

--- src/main/kotlin/com/oksusu/susu/auth/application/DevOAuthService.kt
+++ src/main/kotlin/com/oksusu/susu/auth/application/DevOAuthService.kt
@@ -1,28 +1,28 @@
 package com.oksusu.susu.auth.application
 
-import com.oksusu.susu.auth.helper.KakaoOauthHelper
 import com.oksusu.susu.auth.model.OauthProvider
 import com.oksusu.susu.auth.model.dto.response.OauthLoginLinkResponse
 import com.oksusu.susu.auth.model.dto.response.OauthTokenResponse
+import com.oksusu.susu.client.oauth.kakao.KakaoOauthService
 import org.springframework.stereotype.Service
 
 @Service
 class DevOAuthService(
-    private val kakaoOauthHelper: KakaoOauthHelper,
+    private val kakaoOauthService: KakaoOauthService,
 ) {
     private val logger = mu.KotlinLogging.logger { }
 
     /** oauth login link 가져오기 */
     suspend fun getOauthLoginLinkDev(provider: OauthProvider): OauthLoginLinkResponse {
         return when (provider) {
-            OauthProvider.KAKAO -> kakaoOauthHelper.getOauthLoginLinkDev()
+            OauthProvider.KAKAO -> kakaoOauthService.getOauthLoginLinkDev()
         }
     }
 
     /** oauth token 가져오기 */
     suspend fun getOauthTokenDev(provider: OauthProvider, code: String): OauthTokenResponse {
         return when (provider) {
-            OauthProvider.KAKAO -> kakaoOauthHelper.getOauthTokenDev(code)
+            OauthProvider.KAKAO -> kakaoOauthService.getOauthTokenDev(code)
         }
     }
 }
