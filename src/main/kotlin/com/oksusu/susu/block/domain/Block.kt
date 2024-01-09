21fc63e6dbe8a08d5c26dea1326bfbedd05d917c
wjdtkdgns <wjdtkdgns10266@gmail.com>
2024-01-09 21:49:49 +0900

    feat: block 엔티티 구현

---
+++ src/main/kotlin/com/oksusu/susu/block/domain/Block.kt
@@ -0,0 +1,25 @@
+package com.oksusu.susu.block.domain
+
+import com.oksusu.susu.block.domain.vo.BlockTargetType
+import com.oksusu.susu.category.domain.vo.CategoryAssignmentType
+import com.oksusu.susu.common.domain.BaseEntity
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "block")
+class Block(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = -1,
+
+    val uid: Long,
+
+    @Column(name = "target_id")
+    val targetId: Long,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "target_type")
+    val targetType: BlockTargetType,
+
+    val reason: String?,
+) : BaseEntity()
