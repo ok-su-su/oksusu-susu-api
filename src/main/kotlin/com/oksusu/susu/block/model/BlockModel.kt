8cca4a90355340d7b944703b583ae3f364418f1f
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2024-01-11 15:47:35 +0900

    Update BlockModel.kt

--- src/main/kotlin/com/oksusu/susu/block/model/BlockModel.kt
+++ src/main/kotlin/com/oksusu/susu/block/model/BlockModel.kt
@@ -3,6 +3,7 @@ package com.oksusu.susu.block.model
 import com.oksusu.susu.block.domain.vo.BlockTargetType
 import jakarta.persistence.*
 
+// TODO : DTO data class 이용
 class BlockModel(
     val id: Long,
     val uid: Long,
