c9ece4833332c2271ccde01619170b039125f1e1
Sanghoon Jeong <67852689+wjdtkdgns@users.noreply.github.com>
2024-01-20 16:30:05 +0900

    refac: excel refac (MZ-192)
    * feat: IO 부분 coroutine context 변경
    * refac: sheet class 변경

--- src/main/kotlin/com/oksusu/susu/extension/ObjectExtension.kt
+++ src/main/kotlin/com/oksusu/susu/extension/ObjectExtension.kt
@@ -3,11 +3,12 @@ package com.oksusu.susu.extension
 import kotlin.reflect.KProperty1
 import kotlin.reflect.full.memberProperties
 
-fun <T> T.getPropertyValues(): Map<String, String> {
-    val properties = this!!::class.memberProperties
+fun <T : Any> T.getPropertyValues(): Map<String, String> {
+    val properties = this::class.memberProperties
 
-    return properties.map { property ->
+    return properties.associate { property ->
         val convertedProperty = property as KProperty1<Any, *>
+
         property.name to convertedProperty.get(this).toString()
-    }.toMap()
+    }
 }
