c686f4d693ca0c060040682e6c5ce727b11e82cc
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2024-01-26 23:34:36 +0900

    fix: 봉투 생성, 수정시 친구 정보 제공

--- src/main/kotlin/com/oksusu/susu/envelope/model/response/CreateAndUpdateEnvelopeResponse.kt
+++ src/main/kotlin/com/oksusu/susu/envelope/model/response/CreateAndUpdateEnvelopeResponse.kt
@@ -2,31 +2,33 @@ package com.oksusu.susu.envelope.model.response
 
 import com.oksusu.susu.envelope.domain.Envelope
 import com.oksusu.susu.envelope.domain.vo.EnvelopeType
+import com.oksusu.susu.friend.domain.Friend
+import com.oksusu.susu.friend.model.FriendModel
 import java.time.LocalDateTime
 
 data class CreateAndUpdateEnvelopeResponse(
     val id: Long = -1,
     val uid: Long,
     val type: EnvelopeType,
-    val friendId: Long,
     val amount: Long,
     val gift: String?,
     val memo: String?,
     val hasVisited: Boolean?,
     val handedOverAt: LocalDateTime,
+    val friend: FriendModel,
 ) {
     companion object {
-        fun from(envelope: Envelope): CreateAndUpdateEnvelopeResponse {
+        fun of(envelope: Envelope, friend: Friend): CreateAndUpdateEnvelopeResponse {
             return CreateAndUpdateEnvelopeResponse(
                 id = envelope.id,
                 uid = envelope.uid,
                 type = envelope.type,
-                friendId = envelope.friendId,
                 amount = envelope.amount,
                 gift = envelope.gift,
                 memo = envelope.memo,
                 hasVisited = envelope.hasVisited,
-                handedOverAt = envelope.handedOverAt
+                handedOverAt = envelope.handedOverAt,
+                friend = FriendModel.from(friend)
             )
         }
     }
