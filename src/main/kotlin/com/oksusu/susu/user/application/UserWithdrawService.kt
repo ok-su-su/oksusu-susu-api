2dec5e78c327fef187cfd1c88259041760dac602
wjdtkdgns <wjdtkdgns10266@gmail.com>
2024-03-01 17:56:49 +0900

    feat: user withdraw DDL, service 구현

---
+++ src/main/kotlin/com/oksusu/susu/user/application/UserWithdrawService.kt
@@ -0,0 +1,16 @@
+package com.oksusu.susu.user.application
+
+import com.oksusu.susu.user.domain.UserWithdraw
+import com.oksusu.susu.user.infrastructure.UserWithdrawRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class UserWithdrawService(
+    private val userWithdrawRepository: UserWithdrawRepository,
+) {
+    @Transactional
+    fun saveSync(userWithdraw: UserWithdraw): UserWithdraw {
+        return userWithdrawRepository.save(userWithdraw)
+    }
+}
