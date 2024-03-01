4a6ca1736e4c3e2903624e24a0ce23f6552df17e
wjdtkdgns <wjdtkdgns10266@gmail.com>
2024-03-01 16:44:40 +0900

    refac: post 삭제 api로 변경

--- src/main/kotlin/com/oksusu/susu/post/application/PostAdminFacade.kt
+++ src/main/kotlin/com/oksusu/susu/post/application/PostAdminFacade.kt
@@ -2,16 +2,16 @@ package com.oksusu.susu.post.application
 
 import arrow.fx.coroutines.parZip
 import com.oksusu.susu.auth.model.AdminUser
-import com.oksusu.susu.auth.model.AuthUser
 import com.oksusu.susu.config.database.TransactionTemplates
 import com.oksusu.susu.event.model.DeleteVoteCountEvent
 import com.oksusu.susu.extension.coExecute
+import com.oksusu.susu.post.domain.vo.PostType
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
 
 @Service
-class VoteAdminFacade(
+class PostAdminFacade(
     private val txTemplates: TransactionTemplates,
     private val postService: PostService,
     private val voteService: VoteService,
@@ -20,9 +20,15 @@ class VoteAdminFacade(
 ) {
     private val logger = KotlinLogging.logger { }
 
-    suspend fun deleteVote(user: AdminUser, id: Long) {
-        logger.info { "${user.uid} admin이 $id vote 삭제 실행 " }
+    suspend fun deletePost(user: AdminUser, type: PostType, id: Long) {
+        logger.info { "${user.uid} admin이 $id $type 삭제 실행 " }
 
+        when (type) {
+            PostType.VOTE -> deleteVote(id)
+        }
+    }
+
+    private suspend fun deleteVote(id: Long) {
         val (vote, options) = parZip(
             { voteService.getVote(id) },
             { voteOptionService.getVoteOptions(id) }
--- src/main/kotlin/com/oksusu/susu/post/presentation/VoteAdminResource.kt
+++ src/main/kotlin/com/oksusu/susu/post/presentation/VoteAdminResource.kt
@@ -1,31 +1,33 @@
 package com.oksusu.susu.post.presentation
 
 import com.oksusu.susu.auth.model.AdminUser
-import com.oksusu.susu.auth.model.AuthUser
 import com.oksusu.susu.config.web.SwaggerTag
 import com.oksusu.susu.extension.wrapVoid
-import com.oksusu.susu.post.application.VoteAdminFacade
+import com.oksusu.susu.post.application.PostAdminFacade
+import com.oksusu.susu.post.domain.vo.PostType
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@Tag(name = SwaggerTag.ADMIN_VOTE_SWAGGER_TAG, description = "어드민용 투표 API")
+@Tag(name = SwaggerTag.ADMIN_VOTE_SWAGGER_TAG, description = "어드민용 게시글 API")
 @RestController
-@RequestMapping(value = ["/api/v1/admin/votes"], produces = [MediaType.APPLICATION_JSON_VALUE])
+@RequestMapping(value = ["/api/v1/admin/posts"], produces = [MediaType.APPLICATION_JSON_VALUE])
 class VoteAdminResource(
-    private val voteAdminFacade: VoteAdminFacade,
+    private val postAdminFacade: PostAdminFacade,
 ) {
     /**
      * 어드민 유저만 실행 가능합니다.
      */
-    @Operation(summary = "투표 삭제하기", tags = [SwaggerTag.ADMIN_SWAGGER_TAG])
+    @Operation(summary = "게시글 삭제하기", tags = [SwaggerTag.ADMIN_SWAGGER_TAG])
     @DeleteMapping("/{id}")
-    suspend fun deleteVote(
+    suspend fun deletePost(
         user: AdminUser,
+        @RequestParam type: PostType,
         @PathVariable id: Long,
-    ) = voteAdminFacade.deleteVote(user, id).wrapVoid()
+    ) = postAdminFacade.deletePost(user, type, id).wrapVoid()
 }
