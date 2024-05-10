babe59198938d59938ff1ec61ce32666f83bef33
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2024-05-10 18:25:11 +0900

    feat: encrypt data converter

---
+++ domain/src/main/kotlin/com/oksusu/susu/domain/common/encrypt/EncryptData.kt
@@ -0,0 +1,19 @@
+package com.oksusu.susu.domain.common.encrypt
+
+import com.oksusu.susu.common.encrypt.Encryptor
+
+data class EncryptData(
+    val encData: String
+) {
+    fun dec(encryptor: Encryptor): String {
+        return encryptor.decrypt(this.encData)
+    }
+
+    companion object {
+        fun enc(plainData: String, encryptor: Encryptor): EncryptData {
+            return EncryptData(
+                encData = encryptor.encrypt(plainData),
+            )
+        }
+    }
+}
