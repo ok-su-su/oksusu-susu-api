d0449f318037d52bb7fd7c90866d6b61e5389f11
Sanghoon Jeong <67852689+wjdtkdgns@users.noreply.github.com>
2024-08-27 00:30:37 +0900

    feat: app 강제 업데이트 판별 기능 추가 (MZ-309) (#84)
    * feat: 버전 체크 기능 추가 및 applicationMetadata 엔티티 변경
    
    * feat: 버전 체크 기능 테스트 추가

--- sql/ddl/metadata/DDL.sql
+++ sql/ddl/metadata/DDL.sql
@@ -2,9 +2,8 @@
 CREATE TABLE `application_metadata`
 (
     `id`                  bigint       NOT NULL AUTO_INCREMENT COMMENT '어플리케이션 설정 정보 id',
-    `application_version` varchar(255) NOT NULL COMMENT '최신 어플리케이션 버전',
-    `forced_update_date`  datetime     NOT NULL COMMENT '강제 업데이트 날짜',
-    `description`         text COMMENT '해당 버전의 주요 기능 설명',
+    `ios_min_support_version` varchar(255) NOT NULL COMMENT 'ios 최소 지원 어플리케이션 버전',
+    `aos_min_support_version` varchar(255) NOT NULL COMMENT 'aos 최소 지원 어플리케이션 버전',
     `is_active`           tinyint      NOT NULL COMMENT '활성화 : 1, 비활성화 : 0',
     `created_at`          datetime DEFAULT CURRENT_TIMESTAMP COMMENT '생성일',
     `modified_at`         datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일',
