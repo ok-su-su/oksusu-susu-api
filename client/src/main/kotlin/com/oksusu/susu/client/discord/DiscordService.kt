ef08a362e98d9c65a3b7e6b2924372eacbcb167d
wjdtkdgns <wjdtkdgns10266@gmail.com>
2024-09-11 17:14:29 +0900

    fix: static discord client 생성 timeout 설정

--- client/src/main/kotlin/com/oksusu/susu/client/discord/DiscordService.kt
+++ client/src/main/kotlin/com/oksusu/susu/client/discord/DiscordService.kt
@@ -11,7 +11,12 @@ import org.springframework.web.reactive.function.client.awaitBody
 internal class DiscordService {
     companion object {
         private const val DISCORD_WEBHOOKS_DOMAIN = "https://discord.com/api/webhooks"
-        private val webClient = WebClientFactory.generate(baseUrl = DISCORD_WEBHOOKS_DOMAIN)
+        private val webClient = WebClientFactory.generate(
+            baseUrl = DISCORD_WEBHOOKS_DOMAIN,
+            connectionTimeoutMillis = 2000,
+            readTimeoutMillis = 2000,
+            writeTimeoutMillis = 2000
+        )
 
         fun sendMessage(message: DiscordMessageModel, token: String) {
             CoroutineScope(Dispatchers.IO).launch {
