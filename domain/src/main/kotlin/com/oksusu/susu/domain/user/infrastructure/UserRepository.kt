26e1f448d748b52fcb10698814f3c5ef3719e126
DongGeon0908 <wrjssmjdhappy@gmail.com>
2026-01-22 01:02:34 +0900

    fix: 이전 로직으로 회귀

--- domain/src/main/kotlin/com/oksusu/susu/domain/user/infrastructure/UserRepository.kt
+++ domain/src/main/kotlin/com/oksusu/susu/domain/user/infrastructure/UserRepository.kt
@@ -15,8 +15,4 @@ interface UserRepository : JpaRepository<User, Long>, UserQRepository {
     fun findByOauthInfo(oauthInfo: OauthInfo): User?
 
     fun countByCreatedAtBetween(startAt: LocalDateTime, endAt: LocalDateTime): Long
-
-    fun existsByNewOAuthId(oauthId: String): Boolean
-
-    fun findByNewOAuthId(oauthId: String): User?
 }
